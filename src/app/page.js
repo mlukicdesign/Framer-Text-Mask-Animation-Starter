c61daef541d290b4e0dadc9f82a67059a24f6bf8
mlukicdesign <mlukicdesign@gmail.com>
2024-01-28 10:56:48 +0800

    updated animation to include image component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 import { useRef } from 'react'
 import { motion } from 'framer-motion'
 import styles from './page.module.css'
@@ -36,24 +37,35 @@ export function MaskText() {
     enter: i => ({y: "0", transition: {duration: 0.75, ease: [0.33, 1, 0.68, 1],  delay: 0.075 * i}})
   }
 
+  const imageAnimation = {
+    initial: {y: "150%"},
+    enter: i => ({y: "0", transition: {duration: 1.2, ease: [0.33, 1, 0.68, 1],  delay: 1 * i}})
+  }
+
   // Adjust to true or false to animate in and out
   // Adjust Threshold to change when animation starts
 
   const { ref, inView, entry } = useInView({
-    threshold: 0.90,
+    threshold: 0.80,
     triggerOnce: true
   });
 
 
   return(
+    <div className={styles.contentContainer}>
     <div ref={ref} className={styles.body}>
       {
         phrases.map( (phrase, index) => {
           return <div key={index} className={styles.lineMask}>
-            <motion.p custom={index} variants={animation} initial="initial" animate={inView ? "enter" : ""}>{phrase}</motion.p>
+            <motion.p custom={index} variants={animation} initial="initial" animate={inView ? "enter" : ''}>{phrase}</motion.p>
           </div>
+    
         })
       }
+    </div>
+      <motion.div className={styles.imageContainer} variants={imageAnimation} initial="initial" animate={inView ? "enter": ''}>
+        <Image src="/resource-database-lZKI2P-J2jE-unsplash.jpg" alt="Picture of the author" width={500} height={550}/>
+      </motion.div>
     </div>
   )
 }
